8eaf31ca1fc79a1fd75f90dcdd5f0976de05a5a0
Jonas H <33072340+the-fridge@users.noreply.github.com>
2017-10-26 23:46:47 +0200

    tidy up, added comments
    just added whitespace to match my current coding style
    also added comments explaining the purpose of the code

--- Remixer.py
+++ Remixer.py
@@ -1,75 +1,83 @@
+# this programm takes a source file of which you have to provide the path to
+# it finds all the words in the file strips the file of them and puts them back in
+# but in a completly random order
+# braces stay in their places also everything which isn't a word
+# it returns the result in a .txt file, that has otherwise the same name as the source file
+# used on a pythonsode-file the result can be quite funny
+
+
 from backrefs import bre
-#https://github.com/facelessuser/sublime-backrefs/blob/master/readme.md
+#https://github.com/facelessuser/sublime-backrefs
 import codecs
 from random import random
 import os
 
 
-def findWords(text):
-    words=[]
-    indices=[]
+def findWords( text ) :
+    words = [ ]
+    indices = [ ]
 
-    iterator=bre.finditer("\p{L}+",text)
+    iterator = bre.finditer( "\p{L}+", text )
 
-    for index in iterator:
-        indices.append(index.span())
+    for index in iterator :
+        indices.append( index.span( ) )
 
-    for word in indices:
-        words.append(text[word[0]:word[1]])
+    for word in indices :
+        words.append( text[ word[ 0 ] : word[ 1 ] ] )
 
     return words, indices
 
 
-def mixer(wList, iList,text):
-    parts = len(wList)
-    vorlage=text
-    text=""
-    l=len(iList)
+def mixer( wList, iList, text) :
+    parts = len( wList )
+    vorlage = text
+    text = ""
+    l = len( iList )
 
-    while parts!=0:
-        ran=int(random()*parts)
+    while parts > 0 :
+        ran = int( random( ) * parts )
 
-        if l==parts:
-            text= wList[ran]+vorlage[iList[parts-1][1]:]
-        else:
-            text= wList[ran]+vorlage[iList[parts-1][1]:iList[parts][0]]+text
+        if l == parts :
+            text = wList[ ran ] + vorlage[ iList[ parts - 1 ][ 1 ] : ]
+        else :
+            text = wList[ ran ] + vorlage[ iList[ parts - 1 ][ 1 ] : iList[ parts ][ 0 ] ] + text
             
-        del wList[ran]
-        parts-=1
+        del wList[ ran ]
+        parts -= 1
 
-    text= vorlage[:iList[parts][0]]+text
+    text = vorlage[ : iList[ parts ][ 0 ] ] + text
     
     return text
 
 
-def main():
-    path=input("Dateipfad: ")
+def main( ):
+    path = input( "Dateipfad: " )
     
-    if os.path.isfile(path) == True:
-        print(path, " existiert.")
+    if os.path.isfile( path ) == True :
+        print( path, " existiert." )
         
-        file=codecs.open(path,"r","utf-8")
-        text=file.read()
-        file.close()
+        file = codecs.open( path, "r", "utf-8" )
+        text = file.read( )
+        file.close( )
 
-        fileName, fileExtension = os.path.splitext(path)
+        fileName, fileExtension = os.path.splitext( path )
         
-        fileName=path[:-(len(fileExtension))]+" mix.txt"
-        file=open(fileName,"w")
+        fileName = path[ : -( len( fileExtension ) ) ] + " mix.txt"
+        file = open( fileName, "w" )
         
-        words=findWords(text)[0]
-        indices=findWords(text)[1]
+        words = findWords( text )[ 0 ]
+        indices = findWords( text )[ 1 ]
         
-        file.write(mixer(words,indices,text))
-        file.close()
+        file.write( mixer( words, indices, text) )
+        file.close( )
 
-        print("Erfolgreich abgeschlossen")
+        print( "Erfolgreich abgeschlossen" )
         
-    else:
-        print(path, " existiert nicht.")
+    else :
+        print( path, " existiert nicht." )
         
-    print()
-    input("Drücke Enter zum Verlassen")
+    print( )
+    input( "Drücke Enter zum Verlassen" )
 
 
-main()
+main( )
